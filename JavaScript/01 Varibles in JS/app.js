a9d6e010f157da2add48f236c02d77630e055809
RafayMemon01 <rafayrashid200611@gmail.com>
2024-12-14 17:18:27 +0500

    js code

---
+++ JavaScript/01 Varibles in JS/app.js
@@ -0,0 +1,16 @@
+// console.log("Variables")
+
+// let num1 = "2"
+// let num2 = "2"
+
+// console.log(num1+num2)
+
+let myName = "Rafay";
+
+console.log("Rafay")
+console.log(myName);
+
+let num1 = 2;
+let num2 = 2;
+
+console.log(num1+num2);
