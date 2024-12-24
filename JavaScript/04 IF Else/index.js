3b470e6aa771100a2b9742914ea614fb647da0fc
RoseTechInstitute <rosetechlyari@gmail.com>
2024-12-24 17:38:50 +0500

    if else

---
+++ JavaScript/04 IF Else/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Node JS")
+
+
+let cityName = "karachi"
+
+let userCityName = "lahore"
+
+if(cityName !== userCityName){
+    console.log("No we are not neighbor")
+}else{
+    console.log("Yes We are neighbors")
+}
