5bd99783766a6c34207a0a0f7a080e347a7c5b86
RoseTechInstitute <rosetechlyari@gmail.com>
2024-12-31 17:39:42 +0500

    post and pre increament

---
+++ JavaScript/06 Unfimiliar Operators/index.js
@@ -0,0 +1,15 @@
+// let num1 = 5; //5+1 = 6, 6+1 = 7
+
+//Post Increament
+
+// console.log(num1++);
+// console.log(num1++);
+// console.log(num1);
+
+//Pre Increament
+
+let num5 = 5;
+
+console.log(++num1)
+console.log(++num1)
+console.log(num1)
