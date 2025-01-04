77d95a2af54d01d089ff3a75a2f585f21d4ae81c
RoseTechInstitute <rosetechlyari@gmail.com>
2025-01-04 18:00:07 +0500

    array hello world

---
+++ JavaScript/09 Array/index.js
@@ -0,0 +1,13 @@
+let teaTypes = ["herbel tea", "", "masala chai"];
+//                  0       ,       1      ,    2
+
+// console.log(typeof(teaTypes[1]))
+
+let a = "apple";
+
+let b = "banana";
+
+let c = "carrot";
+
+let friuts = [a, b, c];
+console.log(friuts[0])
