21ddbd563caee6365c0f33766a70c6ff669d4b46
RoseTechInstitute <rosetechlyari@gmail.com>
2025-01-07 17:26:38 +0500

    class code

--- JavaScript/09 Array/index.js
+++ JavaScript/09 Array/index.js
@@ -1,13 +1,40 @@
-let teaTypes = ["herbel tea", "", "masala chai"];
-//                  0       ,       1      ,    2
+// let teaTypes = ["herbel tea", "", "masala chai"];
+// //                  0       ,       1      ,    2
 
-// console.log(typeof(teaTypes[1]))
+// // console.log(typeof(teaTypes[1]))
 
-let a = "apple";
+// let a = "apple";
 
-let b = "banana";
+// let b = "banana";
 
-let c = "carrot";
+// let c = "carrot";
 
-let friuts = [a, b, c];
-console.log(friuts[0])
+// let friuts = [a, b, c];
+// console.log(friuts[0])
+
+// let animals = ["lion","elephent","bear","camel"]
+
+// console.log(animals)
+
+// animals.push("cat","horse")
+// console.log(animals)
+
+// animals.pop()
+// console.log(animals)
+
+// animals.unshift("hen","deer", "fox")
+// console.log(animals)
+
+// animals.shift()
+
+// console.log(animals)
+
+
+const months = ["jan","feb","april","june"]
+
+months.splice(2,0,'mar')
+
+console.log(months)
+
+months.splice(4,1,"may")
+console.log(months)
